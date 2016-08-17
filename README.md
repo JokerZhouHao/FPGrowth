5462aad71ef5c7f414dca3d024f2c50facc747dc
mogumogu2333 <shangyue1230@gmail.com>
2016-08-17 16:59:24 -0700

    fix some bugs

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 Java implementation of Prof. Jiawei Han's FP growth tree for frequent pattern mining.
 
-Not finished yet.
+
+References
+- Orisun's blog: FP-Tree Implementation
+http://www.cnblogs.com/zhangchaoyang/articles/2198946.html
+- Jiawei Han and Micheline Kamber, Data Mining: Concepts and Techniques. 2nd edition, Morgan Kaufmann, 2006.
--- src/fpgrowth/FPGrow.java
+++ src/fpgrowth/FPGrow.java
@@ -9,26 +9,26 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-
-
+/**
+ * 
+ * @author Yue Shang
+ * 
+ * 
+ *
+ */
 public class FPGrow {
 	FPNode root;
-	int min_sup = 3;
-	HashMap<String, Header> headerTable;
-	Map<List<String>, Integer> frequentMap = new HashMap<List<String>, Integer>();
-//	Map<Set<String>, Integer> frequentMap = new HashMap<Set<String>, Integer>();
+	int min_sup = 5;
+	private Map<List<String>, Integer> frequentMap = new HashMap<List<String>, Integer>();
 	
 	public void FPGrowthAlgorithm(List<List<String>> transactions){
 		HashMap<String, Integer> itemCount = getFreqCount(transactions);
@@ -50,32 +50,13 @@ public class FPGrow {
 		FPGrowth(transactions, null);
 	}
 	
-	/**
-	 * For test, print headers
-	 * @param headers
-	 */
-	private void testHeadTable(HashMap<String, FPNode> headers){
-		if(headers==null) return;
-		for(Entry<String, FPNode> entry : headers.entrySet()){
-			String headerName = entry.getKey();
-			int supp = headers.get(headerName).support;
-			StringBuffer buff = new StringBuffer();
-			FPNode currPointer = entry.getValue().next;
-			while(currPointer!=null){
-				buff.append(currPointer.itemName+"("+currPointer.support+")---->");
-				currPointer = currPointer.next;
-			}
-			
-			System.out.println(headerName+"("+supp+") : "+buff.toString());
-		}
-	}
+	
 	
 	public void getCombinations(ArrayList<FPNode> path, List<List<FPNode>> combinations){
 		if(path==null || path.size()==0)return;
 		int length = path.size();
 		for(int i = 1;i<Math.pow(2, length);i++){
 			String bitmap = Integer.toBinaryString(i);
-//			System.out.println(i+"\t"+bitmap);
 			List<FPNode> combine = new ArrayList<>();
 			for(int j = 0;j<bitmap.length();j++){
 				if(bitmap.charAt(j)=='1'){
@@ -87,7 +68,11 @@ public class FPGrow {
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @param transactions
+	 * @param postModel
+	 */
 	public void FPGrowth(List<List<String>> transactions, List<String> postModel){
 		Map<String, Integer> itemCount = getFreqCount(transactions);
 		Map<String, FPNode> headerTable = new HashMap<>();
@@ -160,17 +145,18 @@ public class FPGrow {
             
             //new conditional pattern base
             List<List<String>> newCPB = new LinkedList<List<String>>();
-            FPNode nextNode = header.next;
+            FPNode nextNode = header;
 			while((nextNode = nextNode.next)!=null){
 				int leaf_supp = nextNode.support;
 				
 				//get the path from root to this node
-				List<String> path = new ArrayList<>();
+				LinkedList<String> path = new LinkedList<>();
 				FPNode parent = nextNode;
 				while(!(parent = parent.parent).itemName.equals("ROOT")){
-					path.add(parent.itemName);
+					path.push(parent.itemName);
 				}
 				if(path.size()==0)continue;
+				
 				while(leaf_supp-- >0){
 					newCPB.add(path);
 				}
@@ -180,7 +166,6 @@ public class FPGrow {
 	}
 	
 	public FPNode buildTree(List<List<String>> transactions, final Map<String, Integer> itemCount, final Map<String, FPNode> headerTable){
-		
 		FPNode root = new FPNode("ROOT");
 		root.parent = null;
 		
@@ -225,8 +210,6 @@ public class FPGrow {
 	                rect = false;
 	                break;
 	            }
-	            
-	            
 	            String childName = root.children.keySet().iterator().next();
 	            root = root.children.get(childName);
 	        }
@@ -249,7 +232,6 @@ public class FPGrow {
 			}
 		}
 		
-//		for(Entry<String, Integer> entry:itemCount.entrySet())System.out.println(entry.getKey()+"\t"+entry.getValue());
 		return itemCount;
 	}
 	
@@ -259,7 +241,6 @@ public class FPGrow {
 		
 		//remove gain= loss=
 		Pattern pattern = Pattern.compile("gain=\\w*|loss=\\w*");
-		
 		
 		String newline;
 		while((newline = br.readLine())!=null){
@@ -274,40 +255,6 @@ public class FPGrow {
 		return transactions;
 	}
 	
-	private void sortItemset(String filename) throws IOException{
-		BufferedReader br = new BufferedReader(new FileReader(new File(filename)));
-		HashMap<String, Integer> itemCount = new HashMap<String, Integer>();
-		
-		String newline;
-		while((newline = br.readLine())!=null){
-			String[] items = newline.split(" ");
-			for(String item:items){
-				if(itemCount.containsKey(item)){
-					int count = itemCount.get(item);
-					itemCount.put(item, ++count);
-				}
-				else{
-					itemCount.put(item, 1);
-				}
-			}
-		}
-		br.close();
-	}
-	
-	public void getPath(String item){
-		FPNode firstNode = this.headerTable.get(item).next;
-		while(firstNode!=null){
-			Stack<FPNode> stack = new Stack<FPNode>();
-			FPNode tail = firstNode;
-			while(!tail.itemName.equals("$")){
-				stack.push(tail);
-				tail = tail.parent;
-			}
-			while(!stack.isEmpty())System.out.print(stack.pop().itemName+"\t");
-			System.out.println();
-			firstNode = firstNode.next;
-		}
-	}
 	
 	
 	
@@ -327,28 +274,36 @@ public class FPGrow {
 		}
 	}
 	
-	public void printHeadTable(){
-		if(this.headerTable==null)return;
-		Iterator<String> it = this.headerTable.keySet().iterator();
-		while(it.hasNext()){
-			String itemName = it.next();
-			int sup = this.headerTable.get(itemName).support;
-//			System.out.println(itemName+"\t"+sup);
-			FPNode node = headerTable.get(itemName).next;
-			System.out.print(sup+" : "+itemName+" : ");
-			while(node!=null){
-				System.out.print(node.toString()+"\t");
-				node = node.next;
+	
+	
+	/**
+	 * For test, print headers
+	 * @param headers
+	 */
+	private void testHeadTable(HashMap<String, FPNode> headers){
+		if(headers==null) return;
+		for(Entry<String, FPNode> entry : headers.entrySet()){
+			String headerName = entry.getKey();
+			int supp = headers.get(headerName).support;
+			StringBuffer buff = new StringBuffer();
+			FPNode currPointer = entry.getValue().next;
+			while(currPointer!=null){
+				buff.append(currPointer.itemName+"("+currPointer.support+")---->");
+				currPointer = currPointer.next;
 			}
-			System.out.println();
+			
+			System.out.println(headerName+"("+supp+") : "+buff.toString());
 		}
 	}
 	
-	
-	
-	public void printResult(){
+	/**
+	 * test only
+	 * @param minLength
+	 */
+	private void printResult(int minLength){
 		for(Entry<List<String>, Integer> entry : this.frequentMap.entrySet()){
 			List<String> rule = entry.getKey();
+			if(rule.size()<minLength)continue;
 			Integer support = entry.getValue();
 			System.out.println(Arrays.toString(rule.toArray())+"\t\t"+support);
 		}
@@ -360,98 +315,9 @@ public class FPGrow {
 	public static void main(String[] args) throws IOException {
 		String infile = "./data/census-sample20.dat";
 		FPGrow model = new FPGrow();
-//		List<List<String>> transactions = model.loadTransactions(infile);
-//		model.FPGrowthAlgorithm(transactions);
-//		
-//		model.printResult();
-		
-		ArrayList<FPNode> nodes = new ArrayList<>();
-		nodes.add(new FPNode("a"));
-		nodes.add(new FPNode("b"));
-		nodes.add(new FPNode("c"));
-		
-		List<List<FPNode>> combinations = new ArrayList<>();
-		model.getCombinations(nodes, combinations);
-		
-		for(List<FPNode> comb:combinations){
-			System.out.println(Arrays.toString(comb.toArray()));
-		}
-		
-		
-		
+		List<List<String>> transactions = model.loadTransactions(infile);
+		model.FPGrowthAlgorithm(transactions);
+		model.printResult(2);
 	}
 }
 
-class Pair{
-	String name;
-	int count;
-	public Pair(String name, int count) {
-		super();
-		this.name = name;
-		this.count = count;
-	}
-	@Override
-	public String toString() {
-		return "Pair [name=" + name + ", count=" + count + "]";
-	}
-	
-	
-	
-}
-
-class PairComparator implements Comparator<Pair>{
-
-	public int compare(Pair o1, Pair o2) {
-		// TODO Auto-generated method stub
-		if(o1.count < o2.count) return 1;
-		else if(o1.count > o2.count)return -1;
-		return 0;
-	}
-	
-}
-
-class Header{
-	int support;
-	FPNode next;
-	
-	public Header(FPNode firstNode) {
-		// TODO Auto-generated constructor stub
-		this.support=firstNode.support;
-		this.next = firstNode;
-	}
-	
-	
-	/**
-	 * Attach new FPNode to the tail of header table
-	 * @param newNode
-	 * @param support
-	 */
-	public void attach(FPNode newNode){
-		
-//		this.support+=newNode.support;
-		FPNode node = this.next;
-		while(node.next!=null){
-			node = node.next;
-		}
-		node.next = newNode;
-	}
-	
-	public void sumSup(){
-		int sup = 0;
-		FPNode node = this.next;
-		while(node!=null){
-			sup+=node.support;
-			node = node.next;
-		}
-		this.support = sup;
-	}
-
-
-	@Override
-	public String toString() {
-		return "Header [support=" + support + ", next=" + next + "]";
-	}
-	
-	
-	
-}
